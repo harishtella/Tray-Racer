1ace50387b88b4eee6ada34facf7f1a31ac23073
Harish Tella <ht@harishtella.info>
2011-01-31 02:09:15 +0700

    initial lein struct of app

---
+++ src/tray_racer/core.clj
@@ -0,0 +1,3 @@
+(ns tray-racer.core)
+
+(println "all your traced rays are belong to us!")
---
+++ test/tray_racer/test/core.clj
@@ -0,0 +1,6 @@
+(ns tray-racer.test.core
+  (:use [tray-racer.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
