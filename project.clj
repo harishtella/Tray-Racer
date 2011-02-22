f2262eb9b13310d055e6b9837bfea8a3cedfafa8
Harish Tella <ht@harishtella.info>
2011-02-22 08:03:07 +0800

    add code need to draw, but not working yet
    fix bug with iterate-with-end util func

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject tray-racer "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "most best awesome ray tracer"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.gjahad/debug-repl "0.3.1"]]
+                 [org.clojars.gjahad/debug-repl "0.3.1"]
+                 [org.clojars.automata/rosado.processing "1.1.0"]]
   :main tray-racer.core)
 
